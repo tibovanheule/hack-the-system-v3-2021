1c90f575c9eb1d883b9eaa423c77f6658ece4340
gertjanDS <gertjan.desmet54@gmail.com>
2021-11-17 21:19:34 +0100

    Brussel

--- cpp getter - mysql/main.cpp
+++ cpp getter - mysql/main.cpp
@@ -33,6 +33,8 @@ It does require to install Boost*/
 
 #endif
 
+extern "C" size_t decode_html_entities_utf8(char *dest, const char *src);
+
 
 using namespace  std;
 
@@ -41,6 +43,26 @@ static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *use
     		((string*)userp)->append((char*)contents, size * nmemb);
     		return size * nmemb;
 }
+
+
+std::string gen_random(const int len) {
+    static const char alphanum[] =
+        "0123456789"
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz";
+    std::string tmp_s;
+    tmp_s.reserve(len);
+
+    for (int i = 0; i < len; ++i) {
+        tmp_s += alphanum[rand() % (sizeof(alphanum) - 1)];
+    }
+    
+    return tmp_s;
+}
+
+
+
+
 
 int main(int argc, char *argv[])
 {
@@ -54,23 +76,25 @@ int main(int argc, char *argv[])
 
   while(crawl < 14) {
   if(curl) {
-    string url = "http://35.233.25.116/sitemap/hotels/Amsterdam/?page=";
+    string url = "http://35.233.25.116/sitemap/hotels/Brussels/?page=";
     url += to_string(crawl);
     crawl++;
     cout << "URL: "<< url << endl;
     curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+    curl_easy_setopt(curl, CURLOPT_USERAGENT, gen_random(12));
     res = curl_easy_perform(curl);
     
+    cout << "BUFFER" << res << endl;
 
-    cout << "BUFFER:" << readBuffer << endl;
     regex link_regex("class=\"hotellink\" href=\"(.*)\"");
     
     auto link_begin = sregex_iterator(readBuffer.begin(), readBuffer.end(), link_regex);
     auto link_end = sregex_iterator();
 
-
+    cout << "I GOT HERE" << endl;
+    sleep(5);
 	 try {
                             /* Create a connection */
                             sql::Driver *driver = get_driver_instance();
@@ -79,14 +103,63 @@ int main(int argc, char *argv[])
                             /* Connect to the MySQL test database */
                             con->setSchema(DB_SHEMA);
                             sql::PreparedStatement *stmt = con->prepareStatement(
-                                    "insert into data (data) values (?);");
+                                    "insert into hotel (name, rooms, lat, `long`, location) values (?, ?, ?, ?, 'Brussels');");
                             for (sregex_iterator i = link_begin; i != link_end; ++i) {
                                 smatch match = *i;
                                 // group 0, volledige match
                                 // group 1, regex group 1
                                 // ...
                                 string match_str = match.str(1);
-                            stmt->setString(1,match_str  );
+                            string read;
+                            cout << "THIS PAGE IS: "<< read << endl;
+                            string url = "http://35.233.25.116";
+                            curl_easy_setopt(curl, CURLOPT_WRITEDATA, &read);
+                            url += match_str;
+                            cout << url << endl;
+                            curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+                            curl_easy_setopt(curl, CURLOPT_USERAGENT, gen_random(12));
+                            cout << gen_random(12);
+                            res = curl_easy_perform(curl);
+                            cout << "RES IS: " << res << endl;
+                            if (res != 0){
+                                continue;
+                            }
+                            
+                            long http_code = 0;
+                            curl_easy_getinfo (curl, CURLINFO_RESPONSE_CODE, &http_code);
+                            cout << http_code << endl;
+
+                            //decode_html_entities_utf8(read.c_str(), 0);
+                            regex regex("<h5 class=\"card-title text-truncate\">(.*)</h5>");
+                            std::smatch m;
+                            regex_search(read, m, regex);
+                            string hotelNaam = m[1];
+                            stmt->setString(1, hotelNaam.c_str());
+                            std::regex roomRegex("([0-9]+) rooms", std::regex_constants::icase);
+                            std::smatch rm;
+                            regex_search(read, rm , roomRegex);
+                            string roomAantal = rm[1];
+                            stmt->setString(2, roomAantal);
+
+
+                            std::regex lat("lat ([0-9.]*)", std::regex_constants::icase);
+                            std::smatch latm;
+                            regex_search(read, latm, lat);
+                            string latCoords = latm[1];
+                            stmt->setString(3, latCoords);
+
+
+
+                            std::regex mLong("long ([0-9.]*)", std::regex_constants::icase);
+                            std::smatch longm;
+                            regex_search(read, longm, mLong);
+                            string longCoords = longm[1];
+                            stmt->setString(4, longCoords);
+
+
+
+
+
                             stmt->execute();
                             }
                             stmt->close();
